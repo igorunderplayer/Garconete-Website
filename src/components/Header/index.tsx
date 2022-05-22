c91116fb3896fece6aeb88e3ed0416d50fe89fa8
igorunderplayer <igorschug6@gmail.com>
2022-05-22 17:39:19 -0300

    add user on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import { Nav } from '../Nav'
+import { UserContext } from '../../contexts/UserContext'
 
 import styles from './styles.module.css'
 
 const Header: React.FC = () => {
+  const { user } = useContext(UserContext)
   return (
     <header className={styles.container}>
       <Link href="/">
         Garçonete
       </Link>
       <Nav />
+      
+      { !!user && (
+        <Link href="/commands"> { /* temp */ }
+          <div className={styles.user}>
+            {user.username}
+            <div className={styles.userAvatar}>
+              <Image
+                src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=512`}
+                alt="Foto de perfil do usuario"
+                className={styles.userAvatarImage}
+                height={40}
+                width={40}
+              />
+            </div>
+          </div>
+        </Link>
+      )}
     </header>
   )
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import Link from 'next/link'
 
 import styles from './styles.module.css'
+import { UserContext } from '../../contexts/UserContext'
 
 const Nav: React.FC = () => {
+  const { user } = useContext(UserContext)
+  const [authUri, setAuthUri] = useState('https://discord.com/api/oauth2/authorize?client_id=743261970379309108&redirect_uri=http://localhost:3000&response_type=code&scope=identify%20guilds')
+
+  useEffect(() => {
+    setAuthUri(
+      `https://discord.com/api/oauth2/authorize?client_id=743261970379309108&redirect_uri=${window.location.origin}&response_type=code&scope=identify%20guilds`
+    )
+  }, [])
+
+
   return (
     <nav className={styles.container}>
       <ul>
           <li>
             <Link href="/commands">Commands</Link>
           </li>
+          { !user && (
+          <li>
+            <Link href={authUri}>Login</Link>
+          </li> 
+          )}
       </ul>
     </nav>
   )
